b0bef1cba65e7b83a70e8f5cbf7a8f94a51efd44
Smil Raj <smil.thakur@zeuslearning.com>
2024-01-18 12:41:11 +0530

    animations and json separate file added

--- Quantum DashBoard JS/index.js
+++ Quantum DashBoard JS/index.js
@@ -13,190 +13,82 @@ announcement_container.style.display = "none"
 alerts_container.style.display = "none"
 collapse_nav_container.style.display = "none"
 
-const alerts_data = [
 
-    {
-        checked: false,
-        content: "License for Introduction to Algebra has been assigned to your school",
-        timestamp: "15-Sep-2018 at 07:21 pm",
-        sub_content: null
-    },
-    {
-        checked: true,
-        content: "Lesson 3 Practical Worksheet overdue for Amy Santiago",
-        timestamp: "15-sep-2018 at 05:21 pm",
-        sub_content: {
-            title: "Course:",
-            content: "Advanced Mathematics"
+const alertJsonFilePath = "./alertData.json"
+const annoucementJsonFilePath = "./announcementData.json"
+const courseJsonFilePath = "./courseData.json"
+
+let alert_data = []
+let announcement_data = []
+let course_card_data = []
+
+const fetchAlertJson = async () => {
+
+    try {
+        var response = await fetch(alertJsonFilePath);
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
         }
-    },
-    {
-        checked: false,
-        content: "23 new students created",
-        timestamp: "14-Sept-2018 at 01:21 pm",
-        sub_content: null
-    },
-    {
-        checked: false,
-        content: "15 submissions ready for evaluation",
-        timestamp: "13-Sep-2018 at 01:15 pm",
-        sub_content: {
-            title: "Class:",
-            content: "Basics of Algebra"
+
+        var jsonData = await response.json();
+
+        alert_data = jsonData
+        console.log(alert_data)
+        load_alerts()
+    } catch (error) {
+        console.error('Error fetching JSON:', error.message);
+    }
+}
+
+
+
+fetchAlertJson()
+
+
+
+const fetchAnnouncementJson = async () => {
+
+    try {
+        var response = await fetch(annoucementJsonFilePath)
+
+        if (!response.ok) {
+            throw new Error("failed to fetch data")
         }
-    },
-    {
-        checked: false,
-        content: "License for Basic Concepts in Geometry has been assigned to your teacher",
-        timestamp: "15-Sep-2018 at 07:21 pm",
-        sub_content: null
-    },
-    {
-        checked: true,
-        content: "Lesson 3 Practice Worksheet overdue for Sam Diego",
-        timestamp: "14-Sept-2018 at 03:45 pm",
-        sub_content: {
-            title: "Couse:",
-            content: "Advanced Mathematics"
-        }
+
+        var jsonData = await response.json();
+
+        announcement_data = jsonData
+        load_announcements()
+    } catch (error) {
+        console.error("error fetching json", error.message)
     }
 
+}
 
-]
+fetchAnnouncementJson()
 
-const announcement_data = [
+const fetchCourseJson = async () => {
 
-    {
-        user: "Wilson Kumar",
-        checked: true,
-        content: "No classes will be held on 21st Nov",
-        timestamp: "15-Sep-2018 at 07:21 pm",
-        files: 2,
-        sub_content: null
-    },
-    {
-        user: "Samson White",
-        checked: false,
-        content: "Guest lecture on Geometry on 20th September",
-        timestamp: "15-sep-2018 at 07:21 pm",
-        files: 2,
-        sub_content: null
-    },
-    {
-        user: "Wilson Kumar",
-        checked: true,
-        content: "Additional course materials available on request",
-        timestamp: "15-Sept-2018 at 07:21 pm",
-        files: null,
-        sub_content: {
-            title: "Course: ",
-            content: "Mathematics 101"
+    try {
+        var response = await fetch(courseJsonFilePath)
+
+        if (!response.ok) {
+            throw new Error("failed to fetch data")
         }
-    },
-    {
-        user: "Wilson Kumar",
-        checked: false,
-        content: "No classes will be held on 25th Dec",
-        timestamp: "13-Sep-2018 at 01:15 pm",
-        files: 4,
-        sub_content: {
-            title: "Class:",
-            content: "Basics of Algebra"
-        }
-    },
-    {
-        user: "Wilson Kumar",
-        checked: false,
-        content: "Additional course materials available on request",
-        timestamp: "15-Sep-2018 at 07:21 pm",
-        files: 4,
-        sub_content: {
-            title: "Course: ",
-            content: "Mathematics 101"
-        }
-    },
 
+        var jsonData = await response.json();
 
+        course_card_data = jsonData
+        load_course_cards()
+    }
+    catch (error) {
+        console.error("error fetching json", error.message)
+    }
 
-]
+}
 
-const course_card_data = [
-
-    {
-        lessonTitle: "Acceleration",
-        subject: "physics",
-        img: "assets/images/imageMask-1.svg",
-        grade: 7,
-        bonus: 2,
-        favourite: true,
-        expired: false,
-        contents: {
-            units: 4,
-            lessons: 18,
-            topics: 24
-        },
-        class: "Mr. Frank's Class B",
-        stats: {
-            students: 50,
-            duration: "21-Jan-2020 - 21-Aug-2020"
-        }
-    },
-    {
-        lessonTitle: "Displacement, Velocity and Speed",
-        subject: "physics",
-        img: "assets/images/imageMask-2.svg",
-        grade: 6,
-        bonus: 3,
-        favourite: true,
-        expired: false,
-        contents: {
-            units: 2,
-            lessons: 15,
-            topics: 20
-        },
-        class: null,
-        stats: null
-    },
-    {
-        lessonTitle: "Introduction to Biology: Micro organisms and how they affec...",
-        subject: "Biology",
-        img: "assets/images/imageMask.svg",
-        grade: 4,
-        bonus: 1,
-        favourite: true,
-        expired: false,
-        contents: {
-            units: 5,
-            lessons: 16,
-            topics: 22
-        },
-        class: "All Classes",
-        stats: {
-            students: 300,
-        }
-    },
-    {
-        lessonTitle: "Introduction to High School Mathematics",
-        subject: "Mathematics",
-        img: "assets/images/imageMask-3.svg",
-        grade: 8,
-        bonus: 3,
-        favourite: false,
-        expired: true,
-        contents: {
-            units: 5,
-            lessons: 16,
-            topics: 22
-        },
-        class: "Mr. Frank's Class A",
-        stats: {
-            students: 44,
-            duration: "14-Oct-2019 - 20-Oct-2020"
-        }
-    },
-
-]
-
+fetchCourseJson()
 
 const load_course_cards = () => {
 
@@ -394,26 +286,28 @@ const load_announcements = () => {
 
 
 const load_alerts = () => {
-    alerts_data.forEach((alert) => {
+    alert_data.forEach((alert) => {
+
+        console.log(alert)
 
         const alertElement = document.createElement("div")
-        alertElement.className = alert.checked === false ? "not_checked_alert" : "checked_alert"
+        alertElement.className = alert["checked"] === false ? "not_checked_alert" : "checked_alert"
         const alert_body = document.createElement("div")
         alert_body.className = "alert_body"
         alertElement.appendChild(alert_body)
         const alert_text = document.createElement("div")
         alert_text.className = "alert_text"
         const alertImg = document.createElement("div")
-        alertImg.className = alert.checked === false ? "not_done_img" : "done_img"
+        alertImg.className = alert["checked"] === false ? "not_done_img" : "done_img"
 
-        alert_text.innerHTML = alert.content
+        alert_text.innerHTML = alert["content"]
 
 
         alert_body.appendChild(alert_text)
         alert_body.appendChild(alertImg)
 
 
-        if (alert.sub_content !== null) {
+        if (alert["sub_content"] !== null) {
 
 
             const alert_sub_body = document.createElement("div")
@@ -425,16 +319,16 @@ const load_alerts = () => {
 
             alertElement.appendChild(alert_sub_body)
             alert_sub_body.appendChild(alert_sub_body_title)
-            alert_sub_body_title.innerHTML = alert.sub_content.title
+            alert_sub_body_title.innerHTML = alert["sub_content"]["title"]
             alert_sub_body.appendChild(alert_sub_body_content)
-            alert_sub_body_content.innerHTML = alert.sub_content.content
+            alert_sub_body_content.innerHTML = alert["sub_content"]["content"]
 
         }
 
         const alert_timestamp = document.createElement("div")
         alert_timestamp.className = "alert_timestamp"
 
-        alert_timestamp.innerHTML = alert.timestamp
+        alert_timestamp.innerHTML = alert["timestamp"]
 
         alertElement.appendChild(alert_timestamp)
 
@@ -447,7 +341,6 @@ const load_alerts = () => {
 }
 
 
-load_alerts()
 load_announcements()
 load_course_cards()
 
@@ -480,4 +373,25 @@ collapse_btn.addEventListener("mouseover", () => {
     collapse_nav_container.style.display = collapse_nav_container.style.display === "none" ? "block" : "none"
     collapse_btn.src = collapse_nav_container.style.display === "none" ? "assets/icons/hamburger.svg" : "assets/icons/hamburgerSelected.svg"
 
+})
+
+
+announcement_container.addEventListener("mouseleave", () => {
+    console.log("leave")
+    announcement_container.style.display = "none"
+    annoucements_btn.style.backgroundImage = announcement_container.style.display === "none" ? 'url("assets/icons/announcements.svg")' : 'url("assets/icons/announcements_selected.svg")';
+
+})
+
+
+alerts_container.addEventListener("mouseleave", () => {
+    alerts_container.style.display = "none"
+    alert_btn.style.backgroundImage = alerts_container.style.display === "none" ? 'url("assets/icons/alerts.svg")' : 'url("assets/icons/alerts_selected.svg")';
+
+})
+
+
+collapse_nav_container.addEventListener("mouseleave", () => {
+    collapse_nav_container.style.display = "none"
+    collapse_btn.src = collapse_nav_container.style.display === "none" ? "assets/icons/hamburger.svg" : "assets/icons/hamburgerSelected.svg"
 })
